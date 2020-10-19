5f1c3db08b8e9020c0f1063d21f1daf59722101f
ioanac96 <ioanaandraciobanu@gmail.com>
2020-10-19 16:46:00 +0300

    7.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react';
 
 function App() {
+  const [click, setClick] = useState('');
+  const [foxImage, setImage]=useState('');
+
+  const switchImage = () => {
+    const request = new Request("https://randomfox.ca/floof/", {method: 'GET'});
+    fetch(request).then(response => {
+      return response.json();
+    }).then(data => {
+      setImage(data.image);
+      setClick(true);
+    });
+      setClick(false);
+  }
+
+  useEffect(switchImage, []);
+
+  console.log(click);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={switchImage}>Click meh</button>
+      {
+        (click) ? <img src={foxImage}></img> : (click === false && foxImage !== '') ? <img src="https://www.health.org.uk/sites/default/files/styles/landscape_3_2/public/2019-11/nhs_performance_and_waiting_times_thumbnailartboard_10_copy.png?itok=-cxl-7oS"></img> :
+         (click === false && foxImage === '') ? <img src="https://www.health.org.uk/sites/default/files/styles/landscape_3_2/public/2019-11/nhs_performance_and_waiting_times_thumbnailartboard_10_copy.png?itok=-cxl-7oS"></img>:null
+      }
+      
+      
     </div>
   );
 }
